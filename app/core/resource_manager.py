82700e8fbc35dd27b94beb2bc615a95c2414b6c6
zmvictor <zmvictorcruse@gmail.com>
2025-02-23 19:58:04 +0000

    to implement resource group allocation interface

--- app/core/resource_manager.py
+++ app/core/resource_manager.py
@@ -1,19 +1,29 @@
 from abc import ABC, abstractmethod
 from app.models.resource import Resource
-from typing import Any, Tuple, List, TypeAlias, Optional
+from typing import List, TypeAlias, Optional, Generic, TypeVar, Sequence
 import random
 
-class ResourceManager(ABC):
+T = TypeVar('T')
+
+class ResourceManager(ABC, Generic[T]):
     '''
     total_resources = available_resources + allocated_resources
     allocated_resources = preemptible_resources + non_preemptible_resources
     '''
     @abstractmethod
-    def allocatable(self, resource: Resource) -> List[Any]:
+    def allocatable(self, resource: Resource) -> Sequence[T]:
         pass
 
     @abstractmethod
-    def allocate(self, resource: Resource) -> Optional[Any]:
+    def allocatable(self, resource_list: List[Resource]) -> Sequence[T]:
+        pass
+
+    @abstractmethod
+    def allocate(self, resource: Resource) -> Optional[T]:
+        pass
+    
+    @abstractmethod
+    def allocate(self, resource_list: List[Resource]) -> Optional[T]:
         pass
 
 NodeResourceManager: TypeAlias = ResourceManager
@@ -35,11 +45,11 @@ class ClusterResourceManager(ResourceManager):
         return self._name
         
 
-class GlobalResourceManager(ResourceManager):
+class GlobalResourceManager(ResourceManager[ClusterResourceManager]):
     def __init__(self, clusters: List[ClusterResourceManager]):
         self._clusters = clusters
     
-    def allocatable(self, resource: Resource) -> List[ClusterResourceManager]:
+    def allocatable(self, resource: Resource) -> Sequence[ClusterResourceManager]:
         clusters = list(filter(lambda cluster: cluster.allocatable(resource), self._clusters))
         return clusters
     
